a09ae8e4b646331d2d7ea6fd977be0b77d4112ea
aselwilliams <aselwilliams22@gmail.com>
2022-05-21 11:16:23 -0400

    btns added and toggled

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import data from './data';
-import {useState, useEffect} from 'react';
-import './App.css';
+import data from "./data";
+import { useState, useEffect } from "react";
+import "./App.css";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
 function App() {
-  const [questions, setQuestions] =useState(data)
+  const [questions, setQuestions] = useState(data);
+  const [isOpen, setIsOpen] = useState(false);
 
-  console.log(questions)
+  const handleToggle = (id) => {
+    questions.map((item) => item.id === id);
+    setIsOpen(!isOpen);
+  };
   return (
     <div className="container">
-<h3 className='question-header'>Questions and answers about login</h3>
-<div>
-  {questions.map((question)=>{
-    return (
-      <div key={question.id} className='question'>
-      <div className='question-header'>
-      <h4>{question.title}</h4>
-
+      <h3 className="question-header">Questions and answers about login</h3>
+      <div>
+        {questions.map((question) => {
+          return (
+            <div key={question.id} className="question">
+              {isOpen ? (
+                <>
+                  {" "}
+                  <div className="question-header">
+                    <h4>{question.title}</h4>
+                    <FaMinus onClick={() => handleToggle(question.id)} />
+                  </div>
+                  <p>{question.info}</p>
+                </>
+              ) : (
+                <>
+                  <div className="question-header">
+                    <h4>{question.title}</h4>
+                    <FaPlus onClick={() => handleToggle(question.id)} />
+                  </div>
+                </>
+              )}
+            </div>
+          );
+        })}
       </div>
-      <p>{question.info}</p>
-      </div>
-    )
-  })}
-
-</div>
-
     </div>
   );
 }
