2b1250e91cdfb42b1ec6b7f7fab2fc845e4ec627
aselwilliams <aselwilliams22@gmail.com>
2022-05-21 10:43:49 -0400

    displayed data on UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import questions from './data'
+import data from './data';
+import {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [questions, setQuestions] =useState(data)
+
+  console.log(questions)
   return (
-    <div className="App">
+    <div className="container">
+<h3 className='question-header'>Questions and answers about login</h3>
+<div>
+  {questions.map((question)=>{
+    return (
+      <div key={question.id} className='question'>
+      <div className='question-header'>
+      <h4>{question.title}</h4>
+
+      </div>
+      <p>{question.info}</p>
+      </div>
+    )
+  })}
+
+</div>
 
     </div>
   );
